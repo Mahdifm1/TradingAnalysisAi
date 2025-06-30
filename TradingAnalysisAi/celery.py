2673113b01973e87db66da196a753c6227a251cc
Mahdi Lotfian <80mahde@gmail.com>
2025-06-30 19:08:23 +0330

    fix celery.py

--- TradingAnalysisAi/celery.py
+++ TradingAnalysisAi/celery.py
@@ -1,10 +1,13 @@
+import eventlet
+eventlet.monkey_patch()
+
 import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trading_platform.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TradingAnalysisAi.settings')
 
-app = Celery('trading_platform')
+app = Celery('TradingAnalysisAi')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
