437b9a7d9566b77c4d844cb79e883beb59d91140
Mahdi Lotfian <80mahde@gmail.com>
2025-07-01 19:14:40 +0330

    some change in accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import CustomVerifyEmailView
 
 urlpatterns = [
-    path('api/auth/registration/account-confirm-email/<str:key>', CustomVerifyEmailView.as_view(), name='account_confirm_email'),
+    path('registration/account-confirm-email/<str:key>', CustomVerifyEmailView.as_view(), name='account_confirm_email'),
 ]
